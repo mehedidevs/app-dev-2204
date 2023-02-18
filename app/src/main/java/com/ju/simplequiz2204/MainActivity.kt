ec497a9ddbd1c0c3816ef96c0f6d78d610dce86c
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-02-18 20:04:55 +0600

    class 23 Navigation commit

--- app/src/main/java/com/ju/simplequiz2204/MainActivity.kt
+++ app/src/main/java/com/ju/simplequiz2204/MainActivity.kt
@@ -1,22 +1,90 @@
 package com.ju.simplequiz2204
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.ju.simplequiz2204.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar.root)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController: NavController =
+            findNavController(R.id.fragmentContainerView)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.searchFragment,
+                R.id.chatFragment
+
+            ),
+            drawerLayout
+        )
 
 
+        setupActionBarWithNavController(navController, drawerLayout)
 
-
+        navView.setupWithNavController(navController)
 
 
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.homeMenu -> {
+                Toast.makeText(this@MainActivity, "Home Clicked", Toast.LENGTH_LONG).show()
+            }
+            R.id.searchMenu -> {
+                Toast.makeText(this@MainActivity, "Search Clicked", Toast.LENGTH_LONG).show()
+            }
+            R.id.chatMenu -> {
+                Toast.makeText(this@MainActivity, "Chat Clicked", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+
+
+
+
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
