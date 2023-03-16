c6aecab993827536ff47be90549129e026bd21e3
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-03-16 22:07:51 +0600

    class 27 Figma to XML

---
+++ app/src/main/java/com/ju/simplequiz2204/class28/MainActivity28.kt
@@ -0,0 +1,12 @@
+package com.ju.simplequiz2204.class28
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ju.simplequiz2204.R
+
+class MainActivity28 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main28)
+    }
+}
