a003252e1b0c90a156c08f5925718e89acf549e2
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-03-21 22:09:24 +0600

    class 29 firebase auth

---
+++ app/src/main/java/com/ju/simplequiz2204/SignInFragment.kt
@@ -0,0 +1,68 @@
+package com.ju.simplequiz2204
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.ju.simplequiz2204.databinding.FragmentSignInBinding
+
+
+class SignInFragment : Fragment() {
+
+
+    lateinit var binding: FragmentSignInBinding
+
+    lateinit var mAuth: FirebaseAuth
+    private var firebaseUser: FirebaseUser? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        FirebaseApp.initializeApp(requireContext())
+        binding = FragmentSignInBinding.inflate(layoutInflater, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        if (firebaseUser != null) {
+            findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+        }
+
+        binding.registerBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_registerFragment)
+        }
+
+        binding.signInBtn.setOnClickListener {
+            val email = binding.emailEdt.text.toString()
+            val password = binding.passEdt.text.toString()
+            signInUser(email, password)
+        }
+        return binding.root
+    }
+
+    private fun signInUser(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(requireContext(), "${it.exception?.message}", Toast.LENGTH_LONG)
+                        .show()
+                }
+
+            }
+
+    }
+
+
+}
--- app/src/main/java/com/ju/simplequiz2204/class29/AlaponMainActivity.kt
+++ app/src/main/java/com/ju/simplequiz2204/class29/AlaponMainActivity.kt
@@ -1,16 +1,15 @@
-package com.ju.simplequiz2204.class28
+package com.ju.simplequiz2204.class29
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ju.simplequiz2204.R
 
-class MainActivity28 : AppCompatActivity() {
-
+class AlaponMainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main28)
+        setContentView(R.layout.activity_main_c29)
 
 
     }
